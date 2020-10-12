fa2bc86533cee8ebebf5365412c9564de7731cbe
nini-23 <70326161+nini-23@users.noreply.github.com>
2020-10-12 20:09:34 +0530

    Add files via upload

---
+++ lineFrobo.ino
@@ -0,0 +1,32 @@
+void setup() 
+{
+  pinMode(8,INPUT);
+  pinMode(9,INPUT);
+  for(int i=4;i<=7;i++)
+  {
+    pinMode(i,OUTPUT);
+  }
+}
+
+void loop() 
+{
+  int a,b;
+  a=digitalRead(8);
+  b=digitalRead(9);
+  if(a==0 && b==0)
+  {
+    PORTD=0b00000000;
+  }
+  if(a==1 && b==0)
+  {
+    PORTD=0b01000000;
+  }
+  if(a==0 && b==1)
+  {
+    PORTD=0b00010000;
+  }
+  if(a==1 && b==1)
+  {
+    PORTD=0b01010000;
+  }
+}
